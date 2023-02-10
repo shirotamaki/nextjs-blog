5c0ca05b5f81811353e66f46f6bd5cfe99ff0dcc
shirotamaki <shiro.tamaki63@gmail.com>
2023-02-10 22:24:17 +0900

    サードパーティースクリプトをScripコンポーネントを使用し追加した

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Image from "next/image";
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="tamaki"
+  />
+);
 
 export default function Home() {
   return (
@@ -11,7 +22,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
         <p className={styles.description}>
